427927004d3d9d9a49cadb0d7886ee2cbaa6d88b
filloax <filloax@gmail.com>
2023-04-13 12:56:25 +0200

    recap sessione 28, update post acquisti

--- assets/js/homebrew.js
+++ assets/js/homebrew.js
@@ -1,7 +1,5 @@
 const windowMediaQuery = window.matchMedia("(max-width: 800px)")
 
-var currentlyShowing = "";
-
 function onChange(ev) {
     const container = document.querySelector(".card-container");
     console.log("Media query changed:", windowMediaQuery.matches);
@@ -16,18 +14,24 @@ windowMediaQuery.addEventListener("change", onChange);
 addEventListener("load", onChange);
 
 addEventListener("load", function(ev) {
-    this.document.getElementById("showone").addEventListener("change", ev => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    let showone = this.document.getElementById("showone");
+    showone.checked = urlParams.has("showone")
+    showone.addEventListener("change", ev => {
         updateShown()
     });
 
     this.document.querySelectorAll("table.index a").forEach(el => el.addEventListener("click", ev => {
-        currentlyShowing = el.href.split("#").slice(-1)[0]
-        updateShown()
-
         if (isShowingOne()) {
             ev.preventDefault();
+            let currentlyShowing = el.href.split("#").slice(-1)[0];
+            setCurrentCard(currentlyShowing);
+            updateShown();
         }
     }));
+
+    updateShown();
 });
 
 function isShowingOne() {
@@ -36,18 +40,30 @@ function isShowingOne() {
 
 function updateShown() {
     if (isShowingOne()) {
+        let currentlyShowing = getCurrentCard();
+        if (currentlyShowing === "" || !currentlyShowing) {
+            setCurrentCard(document.querySelector(".card").querySelector("h3").id);
+            currentlyShowing = getCurrentCard();
+        }
+
         document.querySelectorAll(".card").forEach(el => {
             el.style.display = "none";
         });
-        if (currentlyShowing !== "") {
-            const current = document.querySelector(`#${currentlyShowing}`).closest(".card");
-            if (current) {
-                current.style.display = "initial";
-            }
+        const current = document.querySelector(`#${currentlyShowing}`).closest(".card");
+        if (current) {
+            current.style.display = "initial";
         }
     } else {
         document.querySelectorAll(".card").forEach(el => {
             el.style.display = "initial";
         });
     }
+}
+
+function getCurrentCard() {
+    return window.location.hash.substring(1);
+}
+
+function setCurrentCard(id) {
+    window.location.hash = id;
 }
